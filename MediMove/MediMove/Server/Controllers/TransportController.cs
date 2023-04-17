b80ffa3945ef51c9755e7a98e8204725b44da670
Kamil Pachuta <pachutakamil@gmail.com>
2023-04-17 21:45:03 +0200

    Synchronize commit.

---
+++ MediMove/MediMove/Server/Controllers/TransportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediMove.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransportController : ControllerBase
+    {
+    }
+}
--- MediMove/MediMove/Server/Data/MediMoveDbContext.cs
+++ MediMove/MediMove/Server/Data/MediMoveDbContext.cs
@@ -25,15 +25,15 @@ namespace MediMove.Server.Data
         }
 
         public DbSet<Patient> Patients { get; set; }
-        public DbSet<Availability> Availabilit { get; set; }
+        public DbSet<Availability> Availabilities { get; set; }
 
         public DbSet<Dispatcher> Dispatchers { get; set; }
 
-        public DbSet<Paramedic> Paramedic { get; set; }
+        public DbSet<Paramedic> Paramedics { get; set; }
 
         public DbSet<PersonalInformation> PersonalInformations { get; set; }
 
-        public DbSet<Rate>  Rates { get; set; }
+        public DbSet<Rate> Rates { get; set; }
 
         public DbSet<Team> Teams { get; set; }
 
