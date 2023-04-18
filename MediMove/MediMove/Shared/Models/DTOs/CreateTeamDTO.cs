a4be5d9a7bdba6047bfd192bf4bec76cb731c5cf
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-04-19 00:50:04 +0200

    Added DTOs

---
+++ MediMove/MediMove/Shared/Models/DTOs/CreateTeamDTO.cs
@@ -0,0 +1,15 @@
+﻿using MediMove.Shared.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediMove.Shared.Models.DTOs
+{
+    public class CreateTeamDTO
+    {
+        public (int, int) Paramedics { get; set; }
+        public DateOnly Day { get; set; }
+    }
+}
