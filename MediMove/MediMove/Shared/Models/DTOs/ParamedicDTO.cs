0be7f2946baff170f49a836630847c8981d3bee3
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-04-19 00:53:21 +0200

    Added Id fields to ParamedicDTO and DispatcherDTO

--- MediMove/MediMove/Shared/Models/DTOs/ParamedicDTO.cs
+++ MediMove/MediMove/Shared/Models/DTOs/ParamedicDTO.cs
@@ -8,6 +8,7 @@ namespace MediMove.Shared.Models.DTOs
 {
     public class ParamedicDTO
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string StreetAddress { get; set; }
