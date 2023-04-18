452845b4eb5a0c6a9a6ff9678bd92427a1414ca9
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-04-18 21:21:09 +0200

    Added DispatcherDTO and ParamedicDTO

---
+++ MediMove/MediMove/Shared/Models/DTOs/DispatcherDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediMove.Shared.Models.DTOs
+{
+    public class DispatcherDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddres { get; set; }
+        public string HouseNumber { get; set; }
+        public int ApartmentNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string StateProvince { get; set; }
+        public string Country { get; set; }
+        public decimal Salary { get; set; }
+        public string BankAccountNumber { get; set; }
+    }
+}
