c78e61d78edd1b35dd94b8bd487449fa2c6e79a1
VoXelGDesign <trynewgame@gmail.com>
2023-04-19 14:59:00 +0200

    Fixed names and namespaces in mapping profile.

--- MediMove/MediMove/Server/MediMoveMappingProfile.cs
+++ MediMove/MediMove/Server/MediMoveMappingProfile.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using MediMove.Shared.Entities;
+using MediMove.Server.Entities;
 using MediMove.Shared.Models.DTOs;
 
 namespace MediMove.Server
@@ -11,7 +11,7 @@ namespace MediMove.Server
             CreateMap<Transport, TransportDTO>()// dodać: phone number.
                 .ForMember(m => m.PatientFirstName, c => c.MapFrom(s => s.Patient.PersonalInformation.FirstName))
                 .ForMember(m => m.PatientLastName, c => c.MapFrom(s => s.Patient.PersonalInformation.LastName))
-                .ForMember(m => m.PatientStreetAddress, c => c.MapFrom(s => s.Patient.PersonalInformation.StreetAddres))
+                .ForMember(m => m.PatientStreetAddress, c => c.MapFrom(s => s.Patient.PersonalInformation.StreetAddress))
                 .ForMember(m => m.PatientHouseNumber, c => c.MapFrom(s => s.Patient.PersonalInformation.HouseNumber))
                 .ForMember(m => m.PatientApartmentNumber, c => c.MapFrom(s => s.Patient.PersonalInformation.ApartmentNumber))
                 .ForMember(m => m.PatientWeight, c => c.MapFrom(s => s.Patient.Weight));
