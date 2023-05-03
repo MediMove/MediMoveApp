16d49d822a78b777b5dbbb38667f434e3d1498a4
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-05-03 22:58:32 +0200

    Simplified EntityNotFoundException constructors to one specific use case

--- MediMove/MediMove/Server/Exceptions/EntityNotFoundException.cs
+++ MediMove/MediMove/Server/Exceptions/EntityNotFoundException.cs
@@ -3,9 +3,6 @@ namespace MediMove.Server.Exceptions
 {
     public class EntityNotFoundException : NotFoundException
     {
-        public EntityNotFoundException(string message) : base(message) { }
-        public EntityNotFoundException(Type entityType) :
-            base($"The {entityType.Name} could not be found.") { }
         public EntityNotFoundException(Type entityType, int entityId) :
             base($"The {entityType.Name} with ID {entityId} could not be found.") { }
     }
