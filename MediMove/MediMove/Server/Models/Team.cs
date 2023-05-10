cb7e49ee802c19709f55c1718900fedc8ec8e1b3
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-05-10 03:12:39 +0200

    Removed ForeignKey annotations

--- MediMove/MediMove/Server/Models/Team.cs
+++ MediMove/MediMove/Server/Models/Team.cs
@@ -8,11 +8,9 @@ namespace MediMove.Server.Models
         public DateTime Day { get; set; }
         virtual public ICollection<Transport> Transports { get; set; }
 
-        [ForeignKey("DriverId")]
         virtual public int? DriverId { get; set; }
         virtual public Paramedic Driver { get; set; }
 
-        [ForeignKey("ParamedicId")]
         virtual public int? ParamedicId { get; set; }
         virtual public Paramedic Paramedic { get; set; }
     }
