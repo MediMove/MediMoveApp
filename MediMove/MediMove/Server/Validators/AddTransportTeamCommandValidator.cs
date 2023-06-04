1ea3640a1117bd495c8d50ec12bf7144827d82d3
VoXelGDesign <trynewgame@gmail.com>
2023-06-04 22:21:05 +0200

    Added validation that checks if team added works when transport is realised.

---
+++ MediMove/MediMove/Server/Validators/AddTransportTeamCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediMove.Server.Application.Availabilities.Commands;
+using MediMove.Server.Application.Transports.Commands;
+using MediMove.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediMove.Server.Validators
+{
+    public class AddTransportTeamCommandValidator : AbstractValidator<AddTransportTeamCommand>
+    {
+        public AddTransportTeamCommandValidator(MediMoveDbContext _dbContext)
+        {
+            RuleFor(x => x.TeamId).GreaterThan(0);
+            RuleFor(x => x.TransportId).GreaterThan(0);
+
+            RuleFor(x=> x)
+                .Custom((x, context) =>
+                {
+                    var transportDate = _dbContext.Transports.FirstOrDefault(t => t.Id == x.TransportId).StartTime;
+                    var teamDate = _dbContext.Teams.FirstOrDefault(t => t.Id == x.TeamId).Day;
+
+                    var isSameDay = teamDate.Day == transportDate.Day &&
+                                    teamDate.Month == transportDate.Month &&
+                                    teamDate.Year == transportDate.Year;
+
+                    if (!isSameDay) context.AddFailure("TeamId", "Date of transport and date when team is working is different");
+                });
+        }
+    }
+}
