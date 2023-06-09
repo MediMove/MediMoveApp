c4e6cd7fb2078943ea73a5e9f5b4f8b8d5f51cde
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-06-09 03:30:53 +0200

    Standardized comments

--- MediMove/MediMove/Server/Application/Availabilities/Commands/CreateAvailabilitiesCommand.cs
+++ MediMove/MediMove/Server/Application/Availabilities/Commands/CreateAvailabilitiesCommand.cs
@@ -7,7 +7,7 @@ namespace MediMove.Server.Application.Availabilities.Commands;
 /// <summary>
 /// Command for creating availabilities.
 /// </summary>
-/// <param name="ParamedicId">id of paramedic as integer</param>
+/// <param name="ParamedicId">ParamedicId as integer</param>
 /// <param name="request">CreateAvailabilitiesRequest</param>
 public record CreateAvailabilitiesCommand(int ParamedicId, CreateAvailabilitiesRequest request) : IRequest<ErrorOr<Unit>>;
     
