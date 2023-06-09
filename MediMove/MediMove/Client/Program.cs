f0b7bd4c082ec77da7ba67ce1577216e164cfd8d
KamilPachuta <pachutakamil@gmail.com>
2023-06-09 14:05:51 +0200

    Temporary login, logout and session saving.

--- MediMove/MediMove/Client/Program.cs
+++ MediMove/MediMove/Client/Program.cs
@@ -1,4 +1,6 @@
 using MediMove.Client;
+using MediMove.Client.temp;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +11,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
+builder.Services.AddScoped<MediMoveAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<MediMoveAuthenticationStateProvider>());
+builder.Services.AddScoped<ErrorResponse>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
---
+++ MediMove/MediMove/Client/temp/MediMoveResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace MediMove.Client.temp
+{
+    public record MediMoveResponse
+    {
+        public HttpResponseMessage? HttpResponse { get; } 
+        public ErrorResponse? ErrorResponse { get; }
+
+        public MediMoveResponse(HttpResponseMessage response)
+        {
+            HttpResponse = response;
+            ErrorResponse = null;
+        }
+
+        public MediMoveResponse(ErrorResponse response)
+        {
+            ErrorResponse = response;
+            HttpResponse = null;
+        }
+    }
+}
