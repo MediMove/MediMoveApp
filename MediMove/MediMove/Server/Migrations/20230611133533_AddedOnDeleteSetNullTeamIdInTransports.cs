e93d2cc8be7145600ebd8502545b709df6d733f7
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-06-11 18:45:26 +0200

    Added migration

---
+++ MediMove/MediMove/Server/Migrations/20230611133533_AddedOnDeleteSetNullTeamIdInTransports.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediMove.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedOnDeleteSetNullTeamIdInTransports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transports_Teams_TeamId",
+                table: "Transports");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Transports",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShiftType",
+                table: "Teams",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Billings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InvoiceDate",
+                value: new DateTime(2023, 6, 9, 15, 35, 33, 631, DateTimeKind.Local).AddTicks(2451));
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ShiftType",
+                value: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transports_Teams_TeamId",
+                table: "Transports",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transports_Teams_TeamId",
+                table: "Transports");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Transports",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ShiftType",
+                table: "Teams",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Billings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "InvoiceDate",
+                value: new DateTime(2023, 6, 9, 15, 14, 54, 789, DateTimeKind.Local).AddTicks(7519));
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ShiftType",
+                value: null);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transports_Teams_TeamId",
+                table: "Transports",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+    }
+}
