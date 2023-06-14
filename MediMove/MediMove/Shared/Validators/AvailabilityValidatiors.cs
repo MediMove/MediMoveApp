81aca8c92395d95a199ec2ae7f0bb2e86ee28325
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-06-15 00:20:06 +0200

    Removed duplicate time trimming

--- MediMove/MediMove/Shared/Validators/AvailabilityValidatiors.cs
+++ MediMove/MediMove/Shared/Validators/AvailabilityValidatiors.cs
@@ -6,7 +6,7 @@ namespace MediMove.Shared.Validators
     public static class AvailabilityValidatiors
     {
         public static bool IsAvailabilityWithinFutureRange(DateTime date) =>
-            date.Date > DateTime.Today.Date && date.Date <= DateTime.Today.AddYears(1);
+            date.Date > DateTime.Today && date.Date <= DateTime.Today.AddYears(1);
 
         public static bool IsBeforeShift(DateTime date, ShiftType? shift) =>
             date.Date == DateTime.Today && (shift ?? ShiftType.Morning).StartTime() > DateTime.Now.TimeOfDay;
