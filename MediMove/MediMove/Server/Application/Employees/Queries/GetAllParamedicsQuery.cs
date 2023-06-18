3233fc390e83bd2204ced71fecd9a10aaff77afb
Dominik Ciołczyk <44479446+Dodek69@users.noreply.github.com>
2023-06-18 18:33:32 +0200

    Added GetAllEmployees

---
+++ MediMove/MediMove/Server/Application/Employees/Queries/GetAllParamedicsQuery.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+using MediatR;
+using MediMove.Shared.Models.DTOs;
+
+namespace MediMove.Server.Application.Employees.Queries;
+
+/// <summary>
+/// Query for getting all paramedics.
+/// </summary>
+/// <param name="IsWorking">specifies whether to filter employees by their working status</param>
+public record GetAllParamedicsQuery(bool? IsWorking = null) : IRequest<ErrorOr<GetAllParamedicsResponse>>;
