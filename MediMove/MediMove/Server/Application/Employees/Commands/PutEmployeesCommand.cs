7e985141d4811d2fe7fe0c3b4b2c1ae33ba46140
VoXelGDesign <trynewgame@gmail.com>
2023-06-19 21:06:48 +0200

    Revert "Revert "merge master into kamil""
    This reverts commit eaa16678cfbcb63176bbbaceba5692e294bf03c2.

---
+++ MediMove/MediMove/Server/Application/Employees/Commands/PutEmployeesCommand.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+using MediatR;
+using MediMove.Shared.Models.DTOs;
+
+namespace MediMove.Server.Application.Employees.Commands;
+
+/// <summary>
+/// Command for updating employees.
+/// </summary>
+/// <param name="Request"><see cref="PutEmployeesRequest"/></param>
+public record PutEmployeesCommand(PutEmployeesRequest Request) : IRequest<ErrorOr<Unit>>;
--- MediMove/MediMove/Shared/Models/DTOs/ParamedicDTO.cs
+++ MediMove/MediMove/Shared/Models/DTOs/ParamedicDTO.cs
@@ -4,5 +4,5 @@ namespace MediMove.Shared.Models.DTOs;
 public record ParamedicDTO : EmployeeDTO
 {
     public bool IsDriver { get; set; }
-    public decimal Rate { get; set; }
+    public decimal PayPerHour { get; set; }
 }
