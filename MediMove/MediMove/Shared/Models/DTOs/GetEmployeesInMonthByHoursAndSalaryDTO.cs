ce2367baa73208d52d8c6f287a7ac1ffda9558e4
BartJal <bartjal879@student.polsl.pl>
2023-06-20 11:15:50 +0200

    Reports added
    Reports added

---
+++ MediMove/MediMove/Shared/Models/DTOs/GetEmployeesInMonthByHoursAndSalaryDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediMove.Shared.Models.DTOs
+{
+
+    public class GetEmployeesInMonthByHoursAndSalaryDTO
+    {
+        public class GetEmployeesInMonthByHoursAndSalaryRow
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string PhoneNumber { get; set; }
+            public decimal SalarySum { get; set; }
+        }
+        public GetEmployeesInMonthByHoursAndSalaryRow[] Rows { get; set; }
+
+    }
+
+}
