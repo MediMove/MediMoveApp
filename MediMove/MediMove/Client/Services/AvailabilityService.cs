16d3c4ec9cdc7a76af4feb1eb8cd07042411dc6b
KamilPachuta <pachutakamil@gmail.com>
2023-06-25 14:01:50 +0200

    Delete and Update Availability working.

--- MediMove/MediMove/Client/Services/AvailabilityService.cs
+++ MediMove/MediMove/Client/Services/AvailabilityService.cs
@@ -8,6 +8,7 @@ using System.Net.Http.Formatting;
 using static MediMove.Shared.Models.DTOs.GetAvailabilitiesForParamedicByDateRangeResponse;
 using System.Net.Http.Json;
 using System;
+using Azure.Core;
 
 namespace MediMove.Client.Services
 {
@@ -40,10 +41,29 @@ namespace MediMove.Client.Services
 
             Console.WriteLine("I'm here auth");
             var response = await _httpClient.SendAsync(request);
-            response.EnsureSuccessStatusCode(); // Rzuca wyjątek w przypadku niepowodzenia
             Console.WriteLine(response.StatusCode);
-            //var result = await response.Content.ReadFromJsonAsync<GetAllPatientsResponse>();
+            response.EnsureSuccessStatusCode(); // Rzuca wyjątek w przypadku niepowodzenia
 
+        }
+
+        public async Task DeleteAvailabilities(HashSet<DateTime> hashSet)
+        {
+            var content = new DeleteAvailabilitiesRequest(hashSet);
+            var baseUri = new Uri(_navigationManager.BaseUri);
+            var requestUri = new Uri(baseUri, "/api/v1/Availability");
+            Console.WriteLine($"I'm here {requestUri}");
+            var request = new HttpRequestMessage(HttpMethod.Delete, requestUri);
+
+            var token = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "token");
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            request.Content = new ObjectContent<DeleteAvailabilitiesRequest>(content, new JsonMediaTypeFormatter()); // to działa ale jest tu dodatkowy nuGet, jak wysłać po prostu klase?
+
+            Console.WriteLine("I'm here auth");
+            var response = await _httpClient.SendAsync(request);
+            Console.WriteLine(response.StatusCode);
+            response.EnsureSuccessStatusCode(); // Rzuca wyjątek w przypadku niepowodz
         }
 
         public async Task<Dictionary<DateTime, DateInfo>> GetAvailabilities()
